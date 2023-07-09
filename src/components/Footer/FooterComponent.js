01cafbe9d5752db8a3303432ac9db2d1e7508879
Nuno Ribeiro <nunomiguelribeiro7@gmail.com>
2023-07-09 01:43:26 +0100

    Created Footer component, added responsiveness to Header and Footer

--- src/components/Footer/FooterComponent.js
+++ src/components/Footer/FooterComponent.js
@@ -1,6 +1,39 @@
+import { Link } from "react-router-dom";
+import './FooterComponent.css';
+
+const doormatNavigationLinks = ['Home', 'About', 'Menu', 'Reservations', 'Order Online', 'Login'];
+const contactLinks = ['Address', 'Phone Number', 'Email'];
+const socialMediaLinks = ['Facebook', 'Instagram'];
+
+const FooterLinks = (props) => {
+    return(
+        <div className="footer-column">
+            <h1>{props.columnName}</h1>
+            <ul>
+                {
+                    props.linksList.map((link) => {
+                        return(
+                            <li key={link}>
+                                <Link to=''className="footer-link">{link}</Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
 const Footer = () => {
     return(
-        <footer></footer>
+        <footer>
+            <div className="footer-img" />
+            <div className="footer-links-container">
+                <FooterLinks columnName='Doormat Navigation' linksList={doormatNavigationLinks} />
+                <FooterLinks columnName='Contacts' linksList={contactLinks} />
+                <FooterLinks columnName='Social Media' linksList={socialMediaLinks} />
+            </div>
+        </footer>
     )
 }
 
--- src/components/Header/HeaderComponent.js
+++ src/components/Header/HeaderComponent.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import './HeaderComponent.css';
 import headerLogo from '../../assets/Asset 16@4x.png';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Header = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
     return(
         <>
             <header>
@@ -10,6 +14,7 @@ const Header = () => {
                     <img src={headerLogo} alt="Little Lemon Restaurant" />
                 </div>
                 <nav>
+                    {/* Desktop NavBar */}
                     <ul class="nav-links">
                         <Link to="/home" className="nav-item">Home</Link>
                         <Link to="/about" className="nav-item">About</Link>
@@ -18,6 +23,26 @@ const Header = () => {
                         <Link to="/order" className="nav-item">Order Online</Link>
                         <Link to="/login" className="nav-item">Login</Link>
                     </ul>
+
+                    {/* Mobile NavBar */}
+                    <div className="nav-hamburger">
+                    <RxHamburgerMenu 
+                        onClick={() => setToggleMenu(true)}
+                    />
+                        {
+                            toggleMenu && (
+                                <div className="nav-hamburger-menu">
+                                    <AiOutlineClose className="nav-hamburger-close" onClick={() => { setToggleMenu(false) }} />
+                                    <Link to="/home" className="nav-item">Home</Link>
+                                    <Link to="/about" className="nav-item">About</Link>
+                                    <Link to="/specials" className="nav-item">Menu</Link>
+                                    <Link to="/reservations" className="nav-item">Reservations</Link>
+                                    <Link to="/order" className="nav-item">Order Online</Link>
+                                    <Link to="/login" className="nav-item">Login</Link>
+                                </div>
+                            )
+                        }
+                    </div>
                 </nav>
             </header>
         </>
